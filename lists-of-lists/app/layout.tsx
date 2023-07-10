a4f86d98dd7110628b091f66ad01efb94138a3b3
Emma Forman Ling <emma@convex.dev>
2023-07-10 13:19:44 -0700

    add auth

--- lists-of-lists/app/layout.tsx
+++ lists-of-lists/app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import ConvexClientProvider from "./ConvexClientProvider";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithAuth } from "convex/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ lists-of-lists/convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://tight-crane-92.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
